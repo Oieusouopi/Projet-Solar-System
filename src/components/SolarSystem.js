0e26ef919a9662bf3357062bb0a9e2506675795f
OiEuSouOPi <rafasc866@gmail.com>
2021-10-29 16:47:56 -0300

    requisito 1 dnv

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import
+
+// class SolarSystem extends React.Component {
+//   render() {
+//     return (
+//     <div></div>
+//         );
+//   }
+// }
+
+// export default SolarSystem;
