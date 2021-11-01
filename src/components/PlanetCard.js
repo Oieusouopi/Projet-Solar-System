54825b899424f27109a82838c8225137321807df
OiEuSouOPi <rafasc866@gmail.com>
2021-11-01 16:32:49 -0300

    requisito 5

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage} = this.props;
+    return (
+      <div data-testid="planet-card">
+          <span data-testid="planet-name">{ planetName }</span>
+          <img src={planetImage} alt={`Planeta ` + planetName}/>
+      </div>
+        );
+    }
+}
+
+export default PlanetCard;
